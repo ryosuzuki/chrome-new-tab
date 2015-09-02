2c652e57a61a21217b49b440240175fdcda6af9f
Ryo Suzuki <ryosuzk@gmail.com>
2015-09-02 12:54:24 -0600

    Fix limit image count

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 updateTime();
 setInterval(updateTime, 1000);
 
-var limit = 0211;
+var limit = 0690;
 var random = Math.floor(Math.random() * limit);
 var fileName = formatNumber(random, 4);
 var imgURL = chrome.extension.getURL("src/img/" + fileName + ".jpg");
