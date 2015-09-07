804b17fefe216623dd5fcfae4f90c4224a46a23f
Ryo Suzuki <ryosuzk@gmail.com>
2015-09-07 00:32:23 -0600

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ var limit = 0690;
 var random = Math.floor(Math.random() * limit);
 var fileName = formatNumber(random, 4);
 var imgURL = chrome.extension.getURL("src/img/" + fileName + ".jpg");
-$('#background').css("background-image", "url('" + imgURL + "')").animate({ opacity: 1 }, { duration: 1800 });
+$('#background').css("background-image", "url('" + imgURL + "')").animate({ opacity: 1 }, { duration: 1000 });
 
 updateTime();
 setInterval(updateTime, 1000);
